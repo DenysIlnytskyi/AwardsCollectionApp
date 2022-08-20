5c11bc0c1bd1eeadbb203c01a12dcfdc50f19ceb
DenysIlnytskyi <380984511271k@gmail.com>
2022-08-20 20:44:01 +0200

    Initial Commit

---
+++ AwardsColectionApp/AwardsColectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsColectionAppApp.swift
+//  AwardsColectionApp
+//
+//  Created by Денис Ільницький on 20/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct AwardsColectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
